eae39aef9c774029a78881463b8deed427645e55
Vishnu Nair <96977850+Vishnunair22@users.noreply.github.com>
2023-10-11 22:26:33 +0530

    Updated Tip Calculator code

--- Basic/tip_calculator.py
+++ Basic/tip_calculator.py
@@ -1,14 +1,16 @@
+# Tip Calculator
+print("Welcom to the Tip Calculator")
 # Get the bill amount from the user as a floating-point number
-bill = float(input("Enter your bill in $: "))
+bill = float(input("Enter your bill in $:\n"))
 
 # Get the number of people to split the bill between as an integer
-numver_of_people = int(input("Enter the number of people to split the bill between: "))
+numver_of_people = int(input("Enter the number of people to split the bill between:\n"))
 
 # Get the tip percentage from the user as an integer
-tip = int(input("Enter the percentage: "))
+tip = int(input("Enter the percentage of bill you would like to give as a tip. Is it 10,12 or 15 %:\n"))
 
 # Calculate the new bill by adding the tip to the original bill
-new_bill = bill + (bill * tip / 100)
+new_bill = bill + round((bill * tip / 100),2)
 
 # Calculate the share of the bill for each person
 share = new_bill / numver_of_people
